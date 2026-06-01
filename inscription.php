e843bbcc0ce59aca2499bb5abe55bf88ae974a1c
Mohamed Rahimi <mohamedrahimi@eduroam-n2ng-145054.local.univ-savoie.fr>
2026-06-01 15:05:29 +0200

    tes

--- inscription.php
+++ inscription.php
@@ -12,7 +12,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]);
+    ]); 
 } catch (PDOException $e) {
     die("Erreur BDD : " . $e->getMessage());
 }
